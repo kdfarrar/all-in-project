b341949032e51e73a5be65a7ea56b12d5e8e3b4e
kdfarrar <92184533+kdfarrar@users.noreply.github.com>
2022-04-14 16:14:41 -0400

    Got favorites button progress back to where it was before.

--- _assets/js/favorite_post.js
+++ _assets/js/favorite_post.js
@@ -1,25 +1,43 @@
-function favoritePost(postInfo) {
+function favoritePost() {
 	var post = document.body;
-	favorites = localStorage.getItem("favorite");
+	post.classList.toggle("favorite"); 		// toggle red border around button
 
-	post.classList.toggle("favorite"); 		// add and remove post from favorites
-	if (post.classList.contains("favorite")) {
-	 	localStorage.setItem("favorite", postInfo);
-	}
-	else if (favorites.contains(postInfo)) {
-		localStorage.removeItem("favorite", postInfo);
-	}
+	//var saveData = getFavorites();
+
+	favoritePost.innerHTML += postInfo;
+
+	//var favPosts = JSON.parse(saveData);
+	var favString = "";
+
+	// if (favPosts.length === 0) {
+	// 	favString += postInfo;
+	// }
+	// else if (favPosts.has(postData) == false) {
+	// 	favPosts += postInfo);
+	// 	favString = JSON.stringify(favPosts);
+	// }
+
+	favoritePost.innerHTML += "<br/>" + favString;		//See what's in favString
+
+	localStorage.removeItem("favorites");
+	localStorage.setItem("favorites", favString);
+
+	console.log(postInfo.author);
+}
+
+function getFavorites() {
+	var savedFavs = localStorage.getItem("favorites");
+	return savedFavs;
 }
 
 function showFavorites() {
-	favorites = localStorage.getItem("favorite");
-	text = "";
+	var myFavorites = document.getElementById("myFavorites");
+	var favText = "";
+	var favPosts = JSON.parse(getFavorites());
 
-	for (var i = 0; i < favorites.length; i++) {
-		text += favorites[i];
+	for (var i = 0; i < favPosts.length; i++) {
+		favText += favPosts[i].title + "<br />";
 	}
 
-	// text += favorites;
-
-	myFavorites.innerHTML += favorites;
+	myFavorites.innerHTML += favPosts;
 }
