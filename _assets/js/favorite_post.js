340f06bc711fe41a8dcd1dda184b55484316f13d
kdfarrar <92184533+kdfarrar@users.noreply.github.com>
2022-04-22 00:32:15 -0400

    Added functionality to check if a post has been favorited.
    Checks if a post is in favorites when the page is loaded. Still unable to remove posts one by one from favorites without deleting everything. GitHub Pages not rendering site correctly.

--- _assets/js/favorite_post.js
+++ _assets/js/favorite_post.js
@@ -1,23 +1,38 @@
-// function checkFavorite() {
-// 	savedFavs = getFavorites();
-//
-// 	if (postInfo in savedFavs) {
-// 		post.classList.add("favorite");
-// 	}
-// }
+function checkFavorite() {
+	var savedFavs = getFavorites();
+	var post = document.body;
+
+	if (savedFavs.includes(postInfo.url)) {
+		post.classList.add("favorite");
+	}
+}
 
 function favoritePost() {
 	var post = document.body;
-	post.classList.toggle("favorite"); 		// toggle red border around button
-
 	var favString = getFavorites();
 
-	if (favString !== null) {
-		favString += "|" + JSON.stringify(postInfo);
+	post.classList.toggle("favorite"); // toggle red border around button
+
+	if (post.classList.contains("favorite")) {
+		if (favString !== null) {
+			favString += "|" + JSON.stringify(postInfo);
+		}
+		else {
+			favString = JSON.stringify(postInfo);
+		}
 	}
-	else {
-		favString = JSON.stringify(postInfo);
-	}
+	// else {
+	// 	favString = favString.split('|');
+	//
+	// 	spliceString = favString;
+	// 	for (i = 0; i < spliceString.length; i++) {
+	// 		if (JSON.parse(spliceString[i]).url !== postInfo.url) {
+	// 			favString += "|" + spliceString.splice(i, 1);
+	// 		}
+	// 	}
+	//
+	// 	favString = JSON.stringify(favString);
+	// }
 
 	localStorage.removeItem("favorites");
 	localStorage.setItem("favorites", favString);
@@ -32,11 +47,12 @@ function showFavorites() {
 	var myFavorites = document.getElementById("myFavorites");
 	var favText = "";
 	var favPosts = getFavorites();
-
 	favPosts = favPosts.split("|");
 
+	// prints each favorite as a title with a link to the post, the author, and publish date
 	for (i = 0; i < favPosts.length; i++) {
-		favText += JSON.parse(favPosts[i]).title + "<br />";
+		favText += "<strong><a href='" + JSON.parse(favPosts[i]).url + "'>" + JSON.parse(favPosts[i]).title + "</a></strong><br />";
+		favText += JSON.parse(favPosts[i]).author + " | " + JSON.parse(favPosts[i]).date + "<br />";
 	}
 
 	myFavorites.innerHTML += favText;
